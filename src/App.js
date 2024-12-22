5cf9fd0f7311a33a22d11dfa62ad6015ca500684
Ravan Mehraliyev 16226 <rmehraliyev16226@ada.edu.az>
2024-12-22 19:01:03 +0400

    changes on recipes json and initial version of RecipePage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 import './App.css';
+import RecipePage from './RecipePage'; // Import the RecipePage component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Recipe Manager App</h1>
+        <p>Explore delicious recipes and learn how to cook them!</p>
       </header>
+
+      {/* Render the RecipePage component */}
+      <RecipePage />
+
     </div>
   );
 }
